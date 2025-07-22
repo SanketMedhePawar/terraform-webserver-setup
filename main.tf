cd884ad89e196a20f2c52b1d497a9b314c3d2572
sanketmedhepawar <sanketmedhe311@gmail.com>
2025-07-22 10:36:16 +0000

    update

---
+++ main.tf
@@ -0,0 +1,77 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"  # Replace with your desired region
+}
+
+
+resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_security_group" "http_server_sg" {
+  name   = "http_server_sg"
+  vpc_id = "vpc-0d58c1e456c858183"
+
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name = "http_server_sg"
+  }
+}
+
+resource "aws_instance" "http_server" {
+  ami                    = "ami-0b32d400456908bf9"
+  key_name               = "mywebserver"
+  instance_type          = "t3.medium"
+  subnet_id              = "subnet-0e0a9570bd083ad3a"
+  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+  associate_public_ip_address = true
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.aws_key_pair)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install httpd -y",
+      "sudo systemctl start httpd",
+      "sudo systemctl enable httpd",
+      "sudo usermod -a -G apache ec2-user",
+      "sudo chmod 755 /var/www/html",
+      "cd /var/www/html",
+      "echo Welcome to Webserver ${self.public_dns} | sudo tee /var/www/html/index.html"
+    ]
+  }
+}
