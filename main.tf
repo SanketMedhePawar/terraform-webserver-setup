44875d7011c8cd1cb712b721e557d6d2a79bd757
SanketMedhePawar <sanketmedhe311@gmail.com>
2025-07-23 14:53:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,9 +64,9 @@ resource "aws_security_group" "http_server_sg" {
 }
 
 resource "aws_instance" "http_server" {
-  ami                         = "ami-0b32d400456908bf9 "
+  ami                         = "ami-0b32d400456908bf9"
   instance_type               = "t3.medium"
-  key_name                    = "mywebserver"  # Using your existing key pair
+  key_name                    = "mywebserver"
   subnet_id                   = data.aws_subnets.default.ids[0]
   vpc_security_group_ids      = [aws_security_group.http_server_sg.id]
   associate_public_ip_address = true
